760c817ce76afbfc7d97ec15278e2ce79301fb4c
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-09-07 23:24:47 +0100

    built frontend and completed logic for date and time

---
+++ script.js
@@ -0,0 +1,16 @@
+const dayOfWeek = document.getElementById("day-of-week");
+const timeUTC = document.getElementById("time");
+
+const day = new Date().toLocaleDateString('en-US', { weekday: "long" });
+
+
+dayOfWeek.innerHTML = day;
+
+setInterval(changeTime, 1000);
+
+function changeTime(){
+    const time = new Date().toLocaleTimeString('en-US');
+    timeUTC.innerHTML = time;
+}
+
+changeTime()
